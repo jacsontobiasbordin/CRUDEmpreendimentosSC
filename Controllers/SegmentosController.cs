8f461e547f2e3263e92bdb2d8111a098905ff811
jacsontobiasbordin <jacson.bordin@gmail.com>
2026-03-09 13:51:45 -0300

    Adicionado endpoint de consulta para os seguimentos

---
+++ Controllers/SegmentosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDEmpreendimentosSC.Enums;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace CRUDEmpreendimentosSC.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SegmentosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var segmentos = Enum.GetValues(typeof(Segmento))
+                .Cast<Segmento>()
+                .Select(s => new
+                {
+                    id = (int)s,
+                    descricao = GetEnumDescription(s)
+                });
+
+            return Ok(segmentos);
+        }
+
+        private string GetEnumDescription(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            var attribute = field?
+                .GetCustomAttribute<DescriptionAttribute>();
+
+            return attribute?.Description ?? value.ToString();
+        }
+    }
+}
